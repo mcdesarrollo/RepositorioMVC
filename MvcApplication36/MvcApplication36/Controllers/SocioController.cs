1cdd0252f1b3a616ef86324e1fd9b1669448f4f0
mcdesarrollo <mcdesarrollo@outlook.com>
2015-12-04 16:00:51 -0500

    Clase Socio

---
+++ MvcApplication36/MvcApplication36/Controllers/SocioController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication36.Models;
+
+namespace MvcApplication36.Controllers
+{
+    public class SocioController : Controller
+    {
+        private SocioContext db = new SocioContext();
+
+        //
+        // GET: /Socio/
+
+        public ActionResult Index()
+        {
+            return View(db.Socios.ToList());
+        }
+
+        //
+        // GET: /Socio/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        //
+        // GET: /Socio/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Socio/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Socios.Add(socio);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(socio);
+        }
+
+        //
+        // GET: /Socio/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        //
+        // POST: /Socio/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Socio socio)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(socio).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(socio);
+        }
+
+        //
+        // GET: /Socio/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Socio socio = db.Socios.Find(id);
+            if (socio == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socio);
+        }
+
+        //
+        // POST: /Socio/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Socio socio = db.Socios.Find(id);
+            db.Socios.Remove(socio);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcApplication36/MvcApplication36/Models/Socio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication36.Models
+{
+    public class Socio
+    {
+        public int ID { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+}
